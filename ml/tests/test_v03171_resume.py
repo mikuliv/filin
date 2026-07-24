861e0fff8dbcd8a4b0fd8530a42de49f1899e81c
mikuliv <ruslan.pockatilov@gmail.com>
2026-07-24 20:45:42 +0700

    Заморозка targeted trial revision 3

--- ml/tests/test_v03171_resume.py
+++ ml/tests/test_v03171_resume.py
@@ -5,6 +5,6 @@ from ml.experiments.v0_3_17_1.resume import verify_code_lock
 
 def test_pre_trial_code_lock_is_complete_and_unchanged() -> None:
     value = verify_code_lock()
-    assert value["lock_revision"] == 2
+    assert value["lock_revision"] == 3
     assert value["locked_artifact_count"] == 4
     assert value["locked_artifacts_unchanged"]
